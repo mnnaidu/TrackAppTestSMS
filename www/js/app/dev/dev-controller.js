69633f3a66f9e93a726ca2d01ecfc4265c15858f
kitty <kitty@kittys-MacBook-Pro.local>
2015-08-22 17:47:41 +0530

    Couch DB Lite integration completed with error

--- www/js/app/dev/dev-controller.js
+++ www/js/app/dev/dev-controller.js
@@ -6,6 +6,8 @@ function devCtrlFn($scope, $log, $timeout, $ionicPopup, apiServices) {
 	$log.log('devCtrl called!');
 	$scope.vm = [];
 	$scope.count = 1;
+	$scope.smsData = {};
+	$scope.transactionData = {};
 	$scope.pushSms = function (pIndex, cIndex) {
 		//$log.log('pIndex', pIndex, '; cIndex', cIndex);
 		//$log.log('selected sms : ', $scope.vm[pIndex].smsList[cIndex].sms);
@@ -15,6 +17,38 @@ function devCtrlFn($scope, $log, $timeout, $ionicPopup, apiServices) {
 		}).then(function (res) {
 			//alert('Popup closed!');
 		});
+		
+		//call couch db functions to store the SMS
+		$scope.smsData = {
+			sender: 'AM-ICICIB',
+			msg: $scope.vm[pIndex].smsList[cIndex].sms
+		};
+		smsReader.parse($scope.smsData, function(transactionData) {
+			$log.log("after parse", transactionData);
+			$scope.transactionData = transactionData;
+			$scope.transactionData.trackType = 'expense';
+			config.db.post($scope.transactionData, function(err, ok) {
+				$log.log("inserted successfully");
+				$log.log('err: ', err);
+				$log.log('ok: ', ok);
+			});
+		}, function(e) {
+			$log.log("error while parse ", e);
+			$scope.transactionData = {};
+			$scope.error = e;
+		});
+
+
+		$scope.doParse = function() {
+			$log.log('Doing  parsing', $scope.smsData);
+
+			var doc = { desc : "test", amount : 1000 };
+			doc.type = "expense"
+			config.db.post(doc, function(err, ok) {
+				$log.log("inserted successfully");
+			});
+
+		};
 	};
 	apiServices.getDevListData().then(function (response) {
 		$scope.vm = response.smsData;
--- www/js/init.js
+++ www/js/init.js
@@ -9,6 +9,44 @@ angular.module('money-tracker', ['ionic', 'controllers', 'services'])
 			if (window.statusBar) {
 				StatusBar.styleLightContent();
 			}
+			
+			/* Init Couch DB */
+			var coax = require("coax"),
+				appDbName = "sms";
+
+			function setupDb(db, cb) {
+				db.get(function(err, res, body){
+					console.log(JSON.stringify(["before create db put", err, res, body]))
+					db.put(function(err, res, body){
+						db.get(cb)
+					})
+				})
+			};
+
+			if (cblite) {
+				cblite.getURL(function(err, url) {
+					if (err) {
+						console.log('db not initialized');
+					} else {
+						window.server = coax(url);
+						var db = coax([url, appDbName]);
+						var setUpDbCb = function(err, info) {
+							if (err) {
+								console.log('err', err, info);
+							} else {
+								window.config = {
+									db: db,
+									s: coax(url)
+								};
+							}
+						};
+						setupDb(db, setUpDbCb);
+					}
+				});
+			} else {
+				console.log('cblite not intilized');
+			}
+
 		});
 	})
 	.config(function ($stateProvider, $urlRouterProvider) {
